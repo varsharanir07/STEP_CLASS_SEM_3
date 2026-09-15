42da0f36ade73d59a6ef4f2535b63ef1a8b94b30
Varsha Rani <varsharani.r.07@gmail.com>
2026-09-15 21:14:25 +0530

    Week 4 Practice & Assignment Solutions

---
+++ src/week4_ap/ProductOfArrayExceptSelf.java
@@ -0,0 +1,26 @@
+package week4_ap;
+
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf {
+    static int[] productExceptSelf(int[] nums) {
+        int n=nums.length;
+        int[] ans=new int[n];
+
+        ans[0]=1;
+        for(int i=1;i<n;i++)
+            ans[i]=ans[i-1]*nums[i-1];
+
+        int suffix=1;
+        for(int i=n-1;i>=0;i--){
+            ans[i]*=suffix;
+            suffix*=nums[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums={1,2,3,4};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+}
---
+++ src/week4_ap/SubarraySumEqualsK.java
@@ -0,0 +1,26 @@
+package week4_ap;
+
+import java.util.HashMap;
+
+public class SubarraySumEqualsK {
+    static int subarraySum(int[] nums,int k){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
+
+        int sum=0,count=0;
+
+        for(int num:nums){
+            sum+=num;
+            if(map.containsKey(sum-k))
+                count+=map.get(sum-k);
+
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        int[] nums={1,1,1};
+        System.out.println(subarraySum(nums,2));
+    }
+}
